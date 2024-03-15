31a61236be4b0fc952bc4bb446808c27a4911fcf
Prateek sharma <ps9665748@gmail.com>
2024-03-15 12:00:14 +0530

    user model update

--- src/lib/database/models/user.model.ts
+++ src/lib/database/models/user.model.ts
@@ -1,10 +1,10 @@
 import { Schema, model, models } from "mongoose";
 
 const ImageSchema = new Schema({
-  clerkId:{type:String, required:true,unique:true},
-  email: {type:String, required:true,unique:true},
-  username: {type:String, required:true,unique:true},
-  photo: {type:String,required:true},
+  clerkId:{type:String},
+  email: {type:String},
+  username: {type:String},
+  photo: {type:String},
   firstName: {type:String},
   lastName: {type:String},
   planId: {type:Number,default:1},
