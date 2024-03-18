58bbeb765d36ef83463d164a7e085bbb204bf019
Prateek sharma <ps9665748@gmail.com>
2024-03-18 08:08:44 +0530

    middleware location change

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/","/api/webhooks/clerk", "/api/webhooks/stripe"],
+  publicRoutes: ["/"],
+  ignoredRoutes: ["/api/webhooks/clerk", "/api/webhooks/stripe"],
 });
 
 export const config = {
